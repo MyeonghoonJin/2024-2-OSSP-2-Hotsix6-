79afc2ea3b5425b0a054293eafee6f83199d93c7
진명훈 <jinmyeonghun@jinmyeonghun-ui-MacBookAir.local>
2024-09-20 03:59:11 +0900

    proto

---
+++ src/main/java/com/example/demo/HotsixApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotsixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotsixApplication.class, args);
+	}
+
+}
